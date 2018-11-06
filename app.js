29240fb2960e920756b883dee7ce83be57fa1360
Luis Araujo <luis.city@gmail.com>
2018-11-06 21:29:12 -0200

    configurado server

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // in sublime
 var express = require('express');
-var port = process.env.PORT || 3000;
+var path = require('path');
+
+var port = process.env.PORT || 8080;
 var app = express();
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
+
+app.use('/static', express.static('static'))
+
 app.listen(port, function () {
  console.log(`App rodando na porta: "${port}"`);
 });
