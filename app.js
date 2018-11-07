6a291752820e3a86fd07eee520cab7136a4390e5
Luis Fernando <luis.city@gmail.com>
2018-11-06 22:18:15 -0200

    forçando atualização heroku

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var port = process.env.PORT || 8080;
 var app = express();
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
